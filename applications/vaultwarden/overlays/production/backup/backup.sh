36efd0693882aa4b4f1c0e67eebe9129939cf6f5
Max Wassiljew <x9z@gmx.net>
2024-09-11 21:54:53 +0200

    asc

--- applications/vaultwarden/overlays/production/backup/backup.sh
+++ applications/vaultwarden/overlays/production/backup/backup.sh
@@ -51,7 +51,7 @@ echo RESTIC_VERSION=$RESTIC_VERSION
 curl -fsS -m 10 --retry 5 $HC_URL/start
 
 # Perform restic backup
-restic backup --host "$HOSTNAME" --tag restic:"$RESTIC_VERSION" --tag vaultwarden:"$VAULTWARDEN_VERSION" /data
+restic backup --host "$HOSTNAME" --tag restic:"$RESTIC_VERSION" --tag vaultwarden:"$VAULTWARDEN_VERSION" /data /shared/vault-backup.json
 curl -fsS -m 10 --retry 5 $HC_URL/$?
 
 # Perform restic check
